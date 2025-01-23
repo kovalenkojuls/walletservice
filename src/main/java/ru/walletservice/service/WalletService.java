e13fc5c476487d03fee7ef61e6e4b9a671c8fb46
kovalenkojuls <kovalenko_jul_s@mail.ru>
2025-01-23 09:45:22 +0300

    fixed transaction

--- src/main/java/ru/walletservice/service/WalletService.java
+++ src/main/java/ru/walletservice/service/WalletService.java
@@ -37,7 +37,6 @@ public class WalletService {
      * @return Новый баланс кошелька после операции.
      * @throws ConcurrentModificationException Если возникли проблемы с получением пессимистической блокировки.
      */
-    @Transactional
     public double operateOnWallet(WalletOperationRequest request) {
         try {
             return performOperateOnWallet(request);
@@ -55,6 +54,7 @@ public class WalletService {
      * @throws WalletInsufficientFundsException Если на кошельке недостаточно средств для выполнения операции снятия.
      * @throws WalletInvalidOperationTypeException Если указан неверный тип операции.
      */
+    @Transactional
     private double performOperateOnWallet(WalletOperationRequest request) {
         Wallet wallet = walletRepository.findByWalletIdAndLock(request.getWalletId());
         if (wallet == null) {
